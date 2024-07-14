7150c39c5970c3f9c395d039b4aa88b8797b10b4
z4fL <zaaaafl654@gmail.com>
2024-07-14 19:47:52 +0700

    first commit

---
+++ android/app/src/main/kotlin/id/zafl/my_ecommerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package id.zafl.my_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:my_ecommerce/app.dart';
+
+
+void main() {
+  runApp(const App());
+}
